81288d2e0f2c49e9bec1f3fa0f568eab804fb08c
Jayanta Bhowmick <jbhowmick.et@gmail.com>
2020-11-04 18:31:59 -0500

    Added data collection script

---
+++ collect_data.py
@@ -0,0 +1,105 @@
+from ai2thor.controller import Controller
+from matplotlib import pyplot as plt
+from matplotlib.collections import PatchCollection
+from matplotlib.patches import Rectangle
+import json
+import itertools
+import numpy as np
+
+REJECT_THRESHOLD = 10
+plot_bb = True
+facecolor='r'
+edgecolor='None'
+alpha=0.5
+
+controller = Controller(scene='FloorPlan28', gridSize=0.25, renderObjectImage=True, agentControllerType='Physics')
+
+event = controller.step('Pass')
+
+center = event.metadata["sceneBounds"]["center"]
+print(center)
+
+bounds = np.array(event.metadata["sceneBounds"]["cornerPoints"])
+
+x_min = np.amin(bounds[:, 0])
+x_max = np.amax(bounds[:, 0])
+y_min = np.amin(bounds[:, 1])
+y_max = np.amax(bounds[:, 1])
+z_min = np.amin(bounds[:, 2])
+z_max = np.amax(bounds[:, 2])
+
+num_iters = 10
+
+floor_types = np.arange(1, 5)
+floor_numbers = np.arange(1, 31)
+
+for floor_type in floor_types:
+    for floor_number in floor_numbers:
+        floor_type = '' if floor_type == '1' else floor_type
+        floor = 'FloorPlan' + str(floor_type) + str(floor_number)
+
+        controller.reset(scene=floor)
+
+        for i in range(num_iters):
+            fig, ax = plt.subplots(1)
+            print("###########################")
+            event = controller.step(action='GetReachablePositions')
+            valid_positions = event.metadata['actionReturn']
+
+            idx = np.random.choice(np.arange(0, len(valid_positions)))
+
+            x = valid_positions[idx]['x']
+            y = valid_positions[idx]['y']
+            z = valid_positions[idx]['z']
+
+            horizon = np.random.normal(loc=2.5, scale=10.0)
+            rotation = 180 * np.random.random_sample() - 90
+
+            controller.step(action='InitialRandomSpawn',
+                            randomSeed=0,
+                            forceVisible=True,
+                            numPlacementAttempts=5,
+                            placeStationary=True)
+
+            event = controller.step(action='TeleportFull',
+                                    x=x,
+                                    y=y,
+                                    z=z,
+                                    rotation=dict(x=0.0, y=rotation, z=0.0),
+                                    horizon=horizon)
+
+            img = event.frame
+
+            bounding_boxes = event.instance_detections2D
+
+            labels = []
+            bb_array = []
+
+            if len(bounding_boxes) <= 10:
+                print("Reject Image")
+                plt.imshow(img)
+                plt.show()
+            else:
+                boxes = []
+                ax.imshow(img)
+
+                for key in bounding_boxes:
+                    label = key.split('|')[0]
+                    labels.append(label)
+                    bounding_box = bounding_boxes[key]
+                    bb_array.append(bounding_box)
+
+                    if plot_bb:
+                        xy = (bounding_box[0], bounding_box[1])
+                        width = bounding_box[2] - bounding_box[0]
+                        height = bounding_box[3] - bounding_box[1]
+
+                        rec = Rectangle(xy, width, height, linewidth=1, edgecolor='r', facecolor='none')
+                        ax.add_patch(rec)
+
+                    print(label, bounding_box)
+
+                plt.show()
+
+            with open('metadata.json', 'w') as outfile:
+                json.dump(event.metadata, outfile)
--- example.py
+++ example.py
@@ -1,23 +1,91 @@
 from ai2thor.controller import Controller
+from matplotlib import pyplot as plt
+import json
+import numpy as np
 
-# Kitchens: FloorPlan1 - FloorPlan30
-# Living rooms: FloorPlan201 - FloorPlan230
-# Bedrooms: FloorPlan301 - FloorPlan330
-# Bathrooms: FloorPLan401 - FloorPlan430
 
-controller = Controller(scene='FloorPlan28', gridSize=0.25)
+controller = Controller(scene='FloorPlan28', gridSize=0.25, renderObjectImage=True)
 
-event = controller.step(action='MoveAhead')
+event = controller.step(
+    'AddThirdPartyCamera',
+    rotation=dict(x=45, y=90, z=0),
+    position=dict(x=-4, y=1.5, z=-1.5),
+    fieldOfView=90
+)
 
-# Numpy Array - shape (width, height, channels), channels are in RGB order
-event.frame
+center = event.metadata["sceneBounds"]["center"]
 
-# Numpy Array in BGR order suitable for use with OpenCV
-event.cv2image
+controller.interact()
 
-# current metadata dictionary that includes the state of the scene
-event.metadata
+print(center)
 
-# shuts down the controller
-controller.stop()
+bounds = np.array(event.metadata["sceneBounds"]["cornerPoints"])
 
+print(bounds)
+
+x_min = np.amin(bounds[:, 0])
+x_max = np.amax(bounds[:, 0])
+y_min = np.amin(bounds[:, 1])
+y_max = np.amax(bounds[:, 1])
+z_min = np.amin(bounds[:, 2])
+z_max = np.amax(bounds[:, 2])
+
+y = y_min + 0.9 * y_max - y_min
+
+num_iters = 1
+
+
+def get_2d_vectors(start, end):
+    return np.array([end[0] - start[0], end[1] - start[1]])
+
+
+def calculate_2d_angle(v1, v2):
+    rad = np.arccos((v1 @ v2) / (np.linalg.norm(v1) * np.linalg.norm(v2)))
+    return rad/np.pi * 180
+
+
+def look_at(camera_pos, target_pos):
+    x_base_vec = np.array((0, z_max - camera_pos[2]))
+    x_target_vec = np.array((target_pos[1] - camera_pos[1], target_pos[2] - camera_pos[2]))
+    x_angle = calculate_2d_angle(x_base_vec, x_target_vec)
+
+    y_base_vec = np.array((x_max - camera_pos[0], 0))
+    y_target_vec = np.array((target_pos[0] - camera_pos[0], target_pos[1] - camera_pos[1]))
+    y_angle = calculate_2d_angle(y_base_vec, y_target_vec)
+
+    return x_angle % 180, (90 - y_angle) % 180
+
+
+for i in range(num_iters):
+    x = x_min + np.random.sample() * (x_max - x_min)
+    z = z_min + np.random.sample() * (z_max - z_min)
+
+    x_angle, y_angle = look_at([x, y, z], [center['x'], center['y'], center['z']])
+
+    event = controller.step(
+        'UpdateThirdPartyCamera',
+        thirdPartyCameraId=0,
+        rotation=dict(x=0, y=90, z=0.),
+        position=dict(x=center['x'], y=center['y'], z=center['z'])
+    )
+
+    a = event.third_party_camera_frames
+
+    plt.imshow(a[0])
+    plt.show()
+
+    bounding_boxes = event.instance_detections2D
+
+    labels = []
+    bb_array = []
+
+    for key in bounding_boxes:
+        label = key.split('|')[0]
+        labels.append(label)
+        bounding_box = bounding_boxes[key]
+        bb_array.append(bounding_box)
+
+        print(label, bounding_box)
+
+    with open('metadata.json', 'w') as outfile:
+        json.dump(event.metadata, outfile)
